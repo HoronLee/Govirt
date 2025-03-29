e8ab727aadbcef09b246a9eb54d165a9a109aaf1
Horon_Lee <59383381+HoronLee@users.noreply.github.com>
2025-03-30 00:42:28 +0800

    初始化数据库

--- main.go
+++ main.go
@@ -4,9 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"gohub/bootstrap"
-	"gohub/pkg/config"
-
 	btsConfig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
@@ -27,6 +26,9 @@ func main() {
 	// new 一个 Gin Engine 实例
 	router := gin.New()
 
+	// 初始化 DB
+	bootstrap.SetupDB()
+
 	// 初始化路由绑定
 	bootstrap.SetupRoute(router)
 
@@ -36,5 +38,4 @@ func main() {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
 	}
-
 }
