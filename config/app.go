e80bfb4e98d2fa9388fef215b9a12ef5be62d003
Horon_Lee <59383381+HoronLee@users.noreply.github.com>
2025-03-29 19:38:17 +0800

    配置方案实现

---
+++ config/app.go
@@ -0,0 +1,35 @@
+// Package config 站点配置信息
+package config
+
+import "gohub/pkg/config"
+
+// init 函数在包初始化时自动执行，用于设置应用的配置信息
+func init() {
+	// 调用 config 包的 Add 方法，添加一个名为 "app" 的配置项
+	config.Add("app", func() map[string]interface{} {
+		// 返回一个包含应用配置信息的 map
+		return map[string]interface{}{
+
+			// 应用名称，从环境变量 APP_NAME 中获取，默认值为 "Gohub"
+			"name": config.Env("APP_NAME", "Gohub"),
+
+			// 当前环境，用以区分多环境，一般为 local, stage, production, test
+			"env": config.Env("APP_ENV", "production"),
+
+			// 是否进入调试模式
+			"debug": config.Env("APP_DEBUG", false),
+
+			// 应用服务端口
+			"port": config.Env("APP_PORT", "3000"),
+
+			// 加密会话、JWT 加密
+			"key": config.Env("APP_KEY", "114514horon1919810lee114514"),
+
+			// 用以生成链接
+			"url": config.Env("APP_URL", "http://localhost:3000"),
+
+			// 设置时区，JWT 里会使用，日志记录里也会使用到
+			"timezone": config.Env("TIMEZONE", "Asia/Shanghai"),
+		}
+	})
+}
