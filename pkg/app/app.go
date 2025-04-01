85c3de151588eab4e12581b892264b55ed3e3ae4
Horon_Lee <59383381+HoronLee@users.noreply.github.com>
2025-04-01 19:09:42 +0800

    JWT 授权

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -4,6 +4,7 @@ package app
 
 import (
 	"gohub/pkg/config"
+	"time"
 )
 
 func IsLocal() bool {
@@ -16,4 +17,10 @@ func IsProduction() bool {
 
 func IsTesting() bool {
 	return config.Get("app.env") == "testing"
+}
+
+// TimenowInTimezone 获取当前时间，支持时区
+func TimenowInTimezone() time.Time {
+	chinaTimezone, _ := time.LoadLocation(config.GetString("app.timezone"))
+	return time.Now().In(chinaTimezone)
 }
